d5c00ac958502b02ddd6fa869329daf795192c71
Tedd <78011950+tedddeptrai@users.noreply.github.com>
2023-08-02 17:06:32 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 echo This script will keep your server running even after crashing!
-title Minecraft Anti Crashing by Tedd
+title BetterRun.bat by Tedd
 :StartServer
 start /wait java -jar YOURSERVER.jar
 echo (%time%) Server closed/crashed... restarting!
